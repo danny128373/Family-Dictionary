0657b4beb2baa4cdb232d480c3cd28f700d73b18
Daniel Meza <daniel.meza29@yahoo.com>
2020-07-13 15:51:30 -0500

    Finished Exercise

---
+++ family_dict.py
@@ -0,0 +1,13 @@
+my_family = {
+    "sister": {
+        "name": "Krista",
+        "age": 42
+    },
+    "mother": {
+        "name": "Cathie",
+        "age": 70
+    }
+}
+
+print(
+    f'My sister\'s name is {my_family["sister"]["name"]}, she is {my_family["sister"]["age"]} years old.')
